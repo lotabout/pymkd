0494155db3970f1735ecfa43a7b622431296449e
zhang_ji <zhang_ji@worksap.co.jp>
2016-02-24 13:55:53 +0800

    tmp commit

--- pymark.py
+++ pymark.py
@@ -1,16 +1,47 @@
 ##!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from inliner import inline
+
+class Node():
+    """A tree node"""
+    def __init__(self, parent):
+        self.parent = parent
+        self.children = []
+
+    def get_first_child(self):
+        """Helper function for accessing first child
+
+        :returns: first child if exists, else return None
+
+        """
+        try:
+            return self.children[0]
+        except:
+            return None
+
+    def get_last_child(self):
+        """Helper function for accessing last child
+
+        :returns: last child if exists, else return None
+
+        """
+        try:
+            return self.children[-1]
+        except:
+            return None
+
+
 # Block
-class Block():
+class Block(Node):
     """A block element"""
 
     # whether we can directly add a line to the end of the block or not
     acceptsLines = False
+    type = 'block'
 
     def __init__(self, parent=None):
-        self.children = []
-        self.parent = parent
+        super(Block, self).__init__(parent)
 
     def can_be_sibling(self, line):
         """Check if a line can be sibling of current element
@@ -32,7 +63,42 @@ class Block():
         """
         return False
 
-    def finalize(self):
+    def finalize(self, parser):
         """Action to be made when a block structure is finalized"""
         pass
-        
+
+    @staticmethod
+    def ends_with_blank_line(self):
+        """Check if current block ends with blank line, descending if needed for lists
+
+        :returns: True if it ends with blank line, else False
+
+        """
+        block = self
+
+
+class Document(Block):
+    """Root block of a document"""
+    type = 'document'
+    def __init__(self, parent=None):
+        super(Document, self).__init__(parent)
+
+    def can_contain(self, block):
+        # a document can contain `list` but not `item` directly
+        # that means an `item` should be wrapped by `list`
+        return block.type != 'list-item'
+
+class List(Block):
+    """A list block that wraps list items"""
+    type = 'list'
+    def __init__(self, parent=None):
+        super(List, self).__init__(parent)
+
+    def can_contain(self, block):
+        # a `list` can contain only `list-items`
+        return block.type == 'list-item'
+
+    def finalize(self, parser):
+        for item in self.children:
+            self.ends_with_blank_line()
+
